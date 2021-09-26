19156704093029cbc072a9e9f52c765923ca4345
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-26 08:42:31 +0300

    Implement content to show in notification

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -10,7 +10,9 @@
 
 //notification request is split into two components. For example, the trigger – when to show the notification – can be a calendar trigger that shows the notification at an exact time, it can be an interval trigger that shows the notification after a certain time interval has lapsed, or it can be a geofence that shows the notification based on the user’s location
 
-//alendar triggers requires learning another new data type called DateComponents
+//calendar triggers requires learning another new data type called DateComponents
+
+//If you want to specify a sound you can create a custom UNNotificationSound object and attach it to the sound property, or just use UNNotificationSound.default
 
 import UserNotifications
 import UIKit
@@ -42,6 +44,20 @@ class ViewController: UIViewController {
 //  2.a trigger (when to show it)
 //  3.a request (the combination of content and trigger.)
     @objc func scheduleLocal() {
+        let ceneter = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late Wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        //you can attach custom actions by specifying categoryIdentifier
+        content.categoryIdentifier = "alarm"
+        
+        //To attach custom data to the notification,
+        //e.g. an internal ID, use the userInfo dictionary property
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+        
         
     }
 
